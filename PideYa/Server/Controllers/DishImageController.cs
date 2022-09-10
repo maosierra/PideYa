f3778acddedb301d60c1dd95e6c8a204bd4e8171
Sparthan419 <barriosjoan@yahoo.com>
2022-09-10 11:24:56 -0500

    API recuperar URL del plato

---
+++ PideYa/Server/Controllers/DishImageController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PideYa.Server.Helpers;
+using PideYa.Shared.Entities;
+
+namespace PideYa.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DishImageController : Controller
+    {
+        private readonly DataContext _context;
+
+        public DishImageController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DishImage/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DishImage>> GetDishImage(int id)
+        {
+            if (_context.DishImages == null)
+            {
+                return NotFound();
+            }
+            var dishImage = await _context.DishImages
+                .Include(d => d.Dish)
+                .FirstOrDefaultAsync(d => d.id == id);
+
+            if (dishImage == null)
+            {
+                return NotFound();
+            }
+
+            return dishImage;
+        }
+    }
+}
