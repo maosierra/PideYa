8105c751233e01670859d94f98792524e6929c9d
Sparthan419 <barriosjoan@yahoo.com>
2022-09-10 10:42:20 -0500

    UploadFiles

---
+++ PideYa/Server/Controllers/UploadController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PideYa.Server.Helpers;
+using System.Net.Http.Headers;
+
+namespace PideYa.Server.Controllers
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class UploadController : Controller
+    {
+        private readonly DataContext context;
+
+        public UploadController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("{id}"), DisableRequestSizeLimit]
+        public IActionResult Upload(int id)
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources", "Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    //var fullPath = Path.Combine(pathToSave, fileName);
+                    var fullPath = Path.Combine(pathToSave, string.Concat(id.ToString(), Path.GetExtension(Path.Combine(pathToSave, fileName))));
+                    var dbPath = Path.Combine(folderName, string.Concat(id.ToString(), Path.GetExtension(Path.Combine(pathToSave, fileName))));
+                    //var dbPath = Path.Combine(folderName, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    var dish = context.Dishes.FirstOrDefault(o => o.id == id);
+                    
+                    if (dish is null) return NotFound();
+
+                    context.DishImages.Add(new Shared.Entities.DishImage()
+                    {
+                        id = id,
+                        Dish = dish,
+                        url = dbPath
+                    });
+
+                    context.SaveChanges();
+
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
---
+++ PideYa/Shared/Entities/DishImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PideYa.Shared.Entities
+{
+    public class DishImage
+    {
+        public int id { get; set; }
+        public Dish Dish { get; set; }
+        public string url { get; set; }
+    }
+}
