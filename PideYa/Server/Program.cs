629ff20ab91251b7eeb653cd9b096d57b632cedf
maosierra <fidodido18@hotmail.com>
2022-09-10 19:58:49 -0500

    Use development exception page in prod

--- PideYa/Server/Program.cs
+++ PideYa/Server/Program.cs
@@ -35,10 +35,11 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
+app.UseDeveloperExceptionPage();
 
 app.UseHttpsRedirection();
 
