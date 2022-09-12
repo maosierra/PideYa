6dadc69e9c0dbf97424ea2806ade4bab365c6354
maosierra <fidodido18@hotmail.com>
2022-09-12 18:52:50 -0500

    Product list, create and delete

--- PideYa/Shared/Entities/Dish.cs
+++ PideYa/Shared/Entities/Dish.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PideYa.Shared.Entities
 {
     public class Dish
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(20, ErrorMessage = "Nombre debe ser menor a 20 caracteres")]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Valor debe ser positivo")]
+        [Range(0.01, 999999999.99, ErrorMessage = "El valor debe ser mayor a 0 y menor que 999999999")]
         public float Price { get; set; }
+        [Required(ErrorMessage = "Seleccione una categoria valida")]
+        [Range(1, 999999, ErrorMessage = "Seleccione una categoria valida")]
         public int CategoryId { get; set; }
         public DishCategory? Category { get; set; }
     }
